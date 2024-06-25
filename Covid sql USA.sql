c47cdc25d7f1e47b5be56c2e80bcd9e037871a58
Nwajagu Stanley <nwajagustanley@gmail.com>
2024-06-26 00:53:17 +0100

    SQL

---
+++ Covid sql USA.sql
@@ -0,0 +1,41 @@
+select * from covid19_usa;
+
+select distinct(monthname(date)) as Month,sum(cases) as "Total cases"from covid19_usa
+group by monthname(date)
+order by sum(cases) desc;
+
+
+select distinct(monthname(date)) as Month,sum(deaths) as "Total deaths"from covid19_usa
+group by monthname(date)
+order by sum(deaths) desc;
+
+select sum(cases) as TotalCases from covid19_usa;
+
+select sum(deaths) as Totaldeaths from covid19_usa;
+
+select date,cases as "Number of Cases" from covid19_usa
+where cases = 0;
+
+select date,deaths as "Number of deaths" from covid19_usa
+where deaths = 0;
+
+select monthname(date) as Month,sum(cases) as "Number of Cases" from covid19_usa
+group by monthname(date)
+order by 2 desc;
+
+select monthname(date) as Month,sum(deaths) as "Number of deaths" from covid19_usa
+group by monthname(date)
+order by 2 desc;
+
+select distinct(monthname(date)) as Month,sum(cases) as Cases,sum(Deaths) as Death,(sum(deaths)/sum(cases))*100 as "Death Percentage"
+from covid19_usa
+group by monthname(date);
+
+select  date,cases,deaths,(cases-deaths) as "Survived" from covid19_usa;
+
+select date,cases,deaths,(deaths/cases)*100 as "percentage Death",((cases-deaths)/cases)*100 as "Percentage Survived" from covid19_usa;
+
+select distinct(monthname(date)) as Month,sum(cases) as Cases,sum(deaths) as Deaths,(sum(deaths)/sum(cases))*100 as "Death Percentage",(sum(cases-deaths)/sum(cases))*100 as "Percentage Survived"
+from covid19_usa
+group by monthname(date);
+
